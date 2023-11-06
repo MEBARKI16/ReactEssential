abf16539a59b85aa9f70dcf9bd9b0261c7789530
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-11-06 15:49:51 +0100

    Deriving & Outputting Data Based on State

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { CORE_CONCEPTS } from './Data'
+import { CORE_CONCEPTS, EXAMPLES } from './Data'
 import Header from './Components/Header';
 import CoreConcept from './Components/CoreConcept.JSX';
 import TabButton from './Components/TabButton'
 import { useState } from 'react';
 function App() {
-  const [topic, setTopic] = useState('Hello')
+  const [topic, setTopic] = useState('components');
   function HandleClick(selectedButton) {
     setTopic(selectedButton)
   }
@@ -24,12 +24,20 @@ function App() {
         <section id='examples'>
           <h2>Exemples</h2>
           <menu>
-            <TabButton onSelect={() => HandleClick("Components")}>Components</TabButton>
-            <TabButton onSelect={() => HandleClick("JSX")}>JSX</TabButton>
-            <TabButton onSelect={() => HandleClick("Props")}>Props</TabButton>
-            <TabButton onSelect={() => HandleClick("State")}>State</TabButton>
+            <TabButton onSelect={() => HandleClick("components")}>Components</TabButton>
+            <TabButton onSelect={() => HandleClick("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => HandleClick("props")}>Props</TabButton>
+            <TabButton onSelect={() => HandleClick("state")}>State</TabButton>
           </menu>
-          {topic}
+          <div id='tab-content'>
+            <h3>{EXAMPLES[topic].title}</h3>
+            <p>{EXAMPLES[topic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[topic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
